c80bf160246dcb4e7b92ebcd0ce881c57aeabbac
James-Fleming12 <jamesfleming.sd@gmail.com>
2025-07-25 20:08:16 -0700

    added batch size for reminder

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from torch import nn
 class LatentPatch(nn.Module):
     # takes some image and returns a sequence of latent tokens that describe it
     # trained contrastively so that each token is similar to the tokens of similar images
-    def __init__(self, image_size, patch_size, latent_dim):
+    def __init__(self, image_size, patch_size, batch_size, latent_dim):
         super.__init__(LatentPatch, self)
         assert(image_size[0] % patch_size[0] == 0, "Patch Size needs to divide Image Size")
         assert(image_size[1] % patch_size[1] == 0, "Patch Size needs to divide Image Size")
         assert(image_size[2] == patch_size[2], "Patch Size channels and Image Size channels need to match")
+        self.batch_size = batch_size
         self.image_size = image_size # size of [H, W, C]
         self.patch_size = patch_size # size of [h, w, C]
         self.latent_dim = latent_dim # size of [d]
