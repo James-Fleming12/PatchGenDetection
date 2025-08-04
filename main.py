8e73c05a93fe32ca64a998d9af3a126ceb747318
James-Fleming12 <jamesfleming.sd@gmail.com>
2025-08-03 20:32:04 -0700

    qol

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from train import trainJEPA, trainMOCO
+from model import ConGenDetect, JepaGenDetect
 
 def main():
-    jepa_model = trainJEPA()
-    moco_model = trainMOCO()
+    jepa_model: JepaGenDetect = trainJEPA()
+    moco_model: ConGenDetect = trainMOCO()
 
 if __name__=="__main__":
     main()
